1991e1a0e3edeeaee8f81e9e3565ee3d3853e8a4
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-11-20 17:55:43 -0600

    Prepared for Heroku push

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       posts, 
-      logged_in: req.session.logged_in 
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -86,6 +86,16 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
